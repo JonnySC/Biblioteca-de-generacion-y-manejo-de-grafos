dd059d95ebd1cb050811d91b211064046515e233
Jonny <54864070+JonnySC@users.noreply.github.com>
2021-04-12 20:50:58 -0500

    Update arista.py

--- proyecto1/arista.py
+++ proyecto1/arista.py
@@ -1,1 +1,12 @@
-class arista 
+class arista:
+    def __init__(self, source, target, id):
+        
+        self.n0 =source
+        self.n1 = target
+        self.id = id
+        
+        
+        
+    def __str__(self):
+        return self.id
+    def
