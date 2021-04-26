9f9b9ff5f0055c408cae1526eb00dbf00aa51b12
Jonny <54864070+JonnySC@users.noreply.github.com>
2021-04-26 10:24:40 -0500

    Update nodo.py

--- proyecto1/nodo.py
+++ proyecto1/nodo.py
@@ -1,1 +1,38 @@
-class nodo 
+class Node:
+
+       def __init__(self, value):
+
+           self.value = value
+
+           self.left = None
+
+           self.right = None
+
+
+       def insert(self, value):
+
+           if value:
+
+               if value < self.value:
+
+                   if self.left is None:
+
+                       self.left = Node(value)
+
+                   else:
+
+                       self.left.insert(value)
+
+               elif value > self.value:
+
+                   if self.right is None:
+
+                       self.right = Node(value)
+
+                   else:
+
+                       self.right.insert(value)
+
+               else:
+
+                   self.value = value
