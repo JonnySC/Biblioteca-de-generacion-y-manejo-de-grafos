42629ec6f3723d9fe7a5f0f67636e41bd14186da
Jonny <54864070+JonnySC@users.noreply.github.com>
2021-04-26 11:58:49 -0500

    Update main.py

--- proyecto1/main.py
+++ proyecto1/main.py
@@ -4,6 +4,6 @@ def print_hi(name):
     print(f'Hi, {name}')  
 
 if __name__ == '__main__':
-    print_hi('Profesor')
+    print_hi('Se esta tranajando actualmente en la Biblioteca "Trabajo en curso" ')
 
 
