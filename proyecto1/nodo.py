3a631d952badb2fab4c0861fb3d5f5761b857331
Jonny <54864070+JonnySC@users.noreply.github.com>
2021-06-20 19:32:13 -0500

    Update nodo.py

--- proyecto1/nodo.py
+++ proyecto1/nodo.py
@@ -1,38 +1,11 @@
-class Node:
-
-       def __init__(self, value):
-
-           self.value = value
-
-           self.left = None
-
-           self.right = None
-
-
-       def insert(self, value):
-
-           if value:
-
-               if value < self.value:
-
-                   if self.left is None:
-
-                       self.left = Node(value)
-
-                   else:
-
-                       self.left.insert(value)
-
-               elif value > self.value:
-
-                   if self.right is None:
-
-                       self.right = Node(value)
-
-                   else:
-
-                       self.right.insert(value)
-
-               else:
-
-                   self.value = value
+class Nodo:
+    #constructor
+    def __init__(self, i):
+        self.id = i
+        self.visitando = False
+        self.nivel = -1
+        self.vecinos = []
+    #Se crea la funcion de agregar vecinos a los nodos
+    def agregaVecino(self, v):
+        if v not in self.vecinos:
+            self.vecinos.append(v)
